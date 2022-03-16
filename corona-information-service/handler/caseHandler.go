fa70a3f817f101183fdbf402b5d4ddb36fd4974d
Ivan Norderhaug <ivann@stud.ntnu.no>
2022-03-16 17:02:05 +0100

    added caseHandler

---
+++ corona-information-service/handler/caseHandler.go
@@ -0,0 +1,11 @@
+package handler
+
+import "net/http"
+
+// CaseHandler */
+func CaseHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not supported. Currently only GET supported.", http.StatusNotImplemented)
+		return
+	}
+}
