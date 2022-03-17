aad8c0c3c8e0d406b47cbd41e07374f115b3760b
Ivan Norderhaug <ivann@stud.ntnu.no>
2022-03-17 15:19:16 +0100

    added stringencyHandler-skeleton and set up handler endpoint in main

---
+++ corona-information-service/handler/stringencyHandler.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"net/http"
+)
+
+func StringencyHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not supported. Currently only GET supported.", http.StatusNotImplemented)
+		return
+	}
+
+}
