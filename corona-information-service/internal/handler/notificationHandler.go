1cef36e60a4fe825c241fb0d866233bf1f9b37e4
Ivan Norderhaug <ivann@stud.ntnu.no>
2022-03-26 23:38:57 +0100

    reworked notification handler

--- corona-information-service/internal/handler/notificationHandler.go
+++ corona-information-service/internal/handler/notificationHandler.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// NotificationHandler */
 func NotificationHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		response, err := tools.RegisterWebhook(r)
@@ -17,53 +18,69 @@ func NotificationHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodGet || r.Method == http.MethodDelete {
-		getWebhookHandler(w, r)
+		pathHandler(w, r)
 	}
 }
 
-func getWebhookHandler(w http.ResponseWriter, r *http.Request) {
+// pathHandler */
+func pathHandler(w http.ResponseWriter, r *http.Request) {
 	//Splits url into parts
 	parts := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
 
 	switch len(parts) {
 	//If length of parts is 4, then it means the user has wants all webhooks
 	case 4:
-		webhooks, err := tools.GetAllWebhooks()
-		if err != nil {
-			http.Error(w, "Error retrieving webhooks from database", http.StatusInternalServerError)
-			return
-		}
-
-		if len(webhooks) == 0 {
-			http.Error(w, "There are currently no webhooks registered in the database", http.StatusNotFound)
-			return
-		}
-
-		tools.Encode(w, webhooks)
+		encodeAllWebhooks(w)
 
 	//If the length of parts is 5, then it means the user has specified webhook id in their request
 	case 5:
 		if r.Method == http.MethodDelete {
-			err := tools.DeleteWebhook(parts[4])
-			if err != nil {
-				http.Error(w, "Error removing webhook from database. it might not exist", http.StatusInternalServerError)
-				return
-			}
-			response := make(map[string]string, 1)
-			response["result"] = "The webhook has been successfully removed from the database!"
-			tools.Encode(w, response)
+			encodeWebhookDeletionResponse(w, parts[4])
 		}
 
 		if r.Method == http.MethodGet {
-			webhook, found := tools.GetWebhook(parts[4])
-			if !found {
-				http.Error(w, "Unable to locate webhook in database", http.StatusNotFound)
-				return
-			}
-			tools.Encode(w, webhook)
+			encodeSingleWebhook(w, parts[4])
 		}
 	default:
 		http.Error(w, "Incorrect path format.", http.StatusBadRequest)
 		return
 	}
+}
+
+// encodeAllWebhooks */
+func encodeAllWebhooks(w http.ResponseWriter) {
+	webhooks, err := tools.GetAllWebhooks()
+	if err != nil {
+		http.Error(w, "Error retrieving webhooks from database", http.StatusInternalServerError)
+		return
+	}
+
+	if len(webhooks) == 0 {
+		http.Error(w, "There are currently no webhooks registered in the database", http.StatusNotFound)
+		return
+	}
+
+	tools.Encode(w, webhooks)
+}
+
+// encodeSingleWebhook */
+func encodeSingleWebhook(w http.ResponseWriter, id string) {
+	webhook, found := tools.GetWebhook(id)
+	if !found {
+		http.Error(w, "Unable to locate webhook in database", http.StatusNotFound)
+		return
+	}
+	tools.Encode(w, webhook)
+}
+
+// encodeWebhookDeletionResponse */
+func encodeWebhookDeletionResponse(w http.ResponseWriter, id string) {
+	err := tools.DeleteWebhook(id)
+	if err != nil {
+		http.Error(w, "Error removing webhook from database. it might not exist", http.StatusInternalServerError)
+		return
+	}
+	response := make(map[string]string, 1)
+	response["result"] = "The webhook has been successfully removed from the database!"
+	tools.Encode(w, response)
 }
