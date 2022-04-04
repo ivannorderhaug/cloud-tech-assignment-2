6285b579407590cb07eb578d2c053c5049b15d10
Ivan Norderhaug <ivann@stud.ntnu.no>
2022-04-04 19:11:12 +0200

    added basic get and put methods for cache

--- corona-information-service/pkg/cache/cache.go
+++ corona-information-service/pkg/cache/cache.go
@@ -4,3 +4,13 @@ package cache
 func New() map[string]interface{} {
 	return make(map[string]interface{}, 0)
 }
+
+// Get item from cache using key
+func Get(cache map[string]interface{}, key string) interface{} {
+	return cache[key]
+}
+
+// Put value in cache by using key
+func Put(cache map[string]interface{}, key, value string) {
+	cache[key] = value
+}
